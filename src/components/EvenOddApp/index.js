b2f95fdd6d7f4176ab0c797787759bc1ecb2dc19
prasannaalllada <prasannaallada9@gmail.com>
2023-01-30 14:12:46 +0000

    even odd app

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,79 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onIncrement = () => {
+    const randomNumber = this.getRandomNumber()
+
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const displayText = count % 2 === 0 ? 'Even' : 'Odd'
+
+    return (
+      <div className="app-container">
+        <div className="count-container">
+          <h1 className="count">Count {count}</h1>
+          <p className="number-category">Count is {displayText}</p>
+          <button
+            type="button"
+            className="increment-button"
+            onClick={this.onIncrement}
+          >
+            Increment
+          </button>
+          <p className="note">*Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
+
+/*
+import './index.css'
+import {Component} from 'react'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onIncerement = () => {
+    const randomNumber = this.getRandomNumber()
+
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const displayCount = count % 2 === 0 ? 'Even' : 'Odd'
+
+    return (
+      <div className="bg-container ">
+        <div className="card-container">
+          <h1 className="heading ">Count {count}</h1>
+          <p className="result">Count is {displayCount}</p>
+          <button type="button" className="btn" onClick={this.onIncerement}>
+            Increment
+          </button>
+          <p className="description">
+            *Increase By Random Number Between 0 to 100
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
+*/
